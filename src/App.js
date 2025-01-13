d87db8951f9674c1084b7de9f22304dba4d971e7
Charity Orhoridiohwo <79048698+CharityO3@users.noreply.github.com>
2025-01-13 19:50:37 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import shecodeslogo from "./shecodeslogo.png";
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={shecodeslogo} className="App-logo img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
